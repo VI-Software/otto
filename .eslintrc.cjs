371e464c1b67f460245b2b7dd74edb51a0c01045
Angel Crujera <miguelangelcrujera@gmail.com>
2025-10-07 19:33:45 +0200

    Update lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+    extends: ['eslint:recommended'],
+    env: {
+        node: true
+    },
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+    },
+    rules: {
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1,
+            },
+        ],
+        'linebreak-style': ['error', 'windows'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'no-var': ['error'],
+        'no-console': [0],
+        'no-control-regex': [0],
+        'no-unused-vars': [
+            'error',
+            {
+                vars: 'all',
+                args: 'none',
+                ignoreRestSiblings: false,
+                argsIgnorePattern: 'reject',
+            },
+        ],
+        'no-async-promise-executor': [0],
+        'no-undef': 0,
+    },
+}
