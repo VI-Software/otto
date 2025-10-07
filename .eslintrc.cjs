73650b2a4679785c12d9768942e56e46dbbb154a
Angel Crujera <miguelangelcrujera@gmail.com>
2025-10-07 19:34:59 +0200

    Update lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
                 SwitchCase: 1,
             },
         ],
-        'linebreak-style': ['error', 'windows'],
+        'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'never'],
         'no-var': ['error'],
